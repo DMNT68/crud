b9e4966cbbe02382c22c6ed8fc51b298f10bf750
DMNT68 <andressalgado41@gmail.com>
2017-12-11 11:04:11 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# crud
+# crud-one
+CRUD con PHP y MySQL
---
+++ php/conexion.php
@@ -0,0 +1,7 @@
+<?php
+$host="us-cdbr-iron-east-05.cleardb.net";
+$user="ba78fed948b3f9";
+$password="4bf7c3d3";
+$db="heroku_bed8d7e563b7b30";
+$con = new mysqli($host,$user,$password,$db);
+?>
